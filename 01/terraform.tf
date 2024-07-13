1eabfd85d47a7103c9e30e47f252d68699467e82
Akash Varshney Personal <akash.varshney@arcadis.com>
2024-07-13 19:59:05 +0100

    Update terraform.tf

--- 01/terraform.tf
+++ 01/terraform.tf
@@ -14,11 +14,10 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
   features {}
-  client_id       = "e596c61b-a0a3-49e4-b2e8-e3821d80eea0" # Add your client_id
-  client_secret   = "qV98Q~dNXog2OCP3bLkJAqdlvaglZuAQvrevObPW" # Add your client_secret
-  tenant_id       = "652c62f1-2766-420a-908a-89717b6465a8" # Add your tenant_id
-  subscription_id = "c9f5db7b-4573-4b0b-9ce2-dedaeef10d7a" # Add your subscription_id
+  client_id       = "xxxxxx-xxxx-xxxx-xxxx-xxxxxx" # Add your client_id
+  client_secret   = "xxxxxx-xxxx-xxxx-xxxx-xxxxxx" # Add your client_secret
+  tenant_id       = "xxxxxx-xxxx-xxxx-xxxx-xxxxxx" # Add your tenant_id
+  subscription_id = "xxxxxx-xxxx-xxxx-xxxx-xxxxxx" # Add your subscription_id
 }
