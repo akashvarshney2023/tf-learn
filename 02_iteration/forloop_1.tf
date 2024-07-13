c2f4fc53595966874bf873f7068c4481c968fdf1
Akash Varshney Personal <akash.varshney@arcadis.com>
2024-07-13 19:51:10 +0100

    Update forloop_1.tf

--- 02_iteration/forloop_1.tf
+++ 02_iteration/forloop_1.tf
@@ -10,23 +10,26 @@ variable "count_details" {
   type = map(number)
   default = {
     # details_key    # details_value
-    "resource_count" = 10
+    "resource_countresource_count" = 10
     "region_count"   = 3
-    "vm_count"       = 8
+    "vm_count"       = 3
   }
 }
 
 
 locals {
-  
-  region_list = [for region in var.regions_list : "${region} - az"]
 
-  ##### Filter the count_details map to include only entries with values greater than 5
- 
+  # [for <item> in <collection>: <expression>]
+   region_list = [for region in var.regions_list : region if region != "canda"]
+
+  # ##### Filter the count_details map to include only entries with values greater than 5
+ ## {for <key>, <value> in <map>: <key_expr> => <value_expr>}
   filtered_count_details = {
     for details_key, details_value in var.count_details : details_key => details_value
-    
+    if details_value > 4
   }
+                                                        #region_count:3
+   newformat = [for key, value in var.count_details : "${key} + ${value}"]
 }
 
 output "count_details" {
@@ -37,5 +40,20 @@ output "server" {
   value = local.region_list
   
 }
+output "new_format" {
+  value = local.newformat
+  
+}
 
 
+# variable "count_details" {
+#   type = map(number)
+#   default = {
+#     # details_key    # details_value
+#     "resource_countresource_count" = 10
+#     "region_count"   = 3
+#     "vm_count"       = 3
+#   }
+# }
+
+###### ["resoruce_count:10","region_count:3", "vm_count:8"]
